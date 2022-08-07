42fe8bf7fab71ea602b34a3336e15dbcb4103fa8
VincentYeh-dev <vincent3314534>
2022-08-08 00:37:31 +0800

    Add gui program.

--- src/main/java/org/vincentyeh/obstextutils/MainProgram.java
+++ src/main/java/org/vincentyeh/obstextutils/MainProgram.java
@@ -1,5 +1,13 @@
 package org.vincentyeh.obstextutils;
 
+import com.formdev.flatlaf.FlatDarkLaf;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.filechooser.FileFilter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
@@ -7,41 +15,148 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class MainProgram {
+    private JPanel root;
+    private JButton browseButton;
+    private JTextField textField_file;
+    private JSlider slider_update_interval;
+    private JTextField textField_format;
+    private JButton startButton;
+    private JButton stopButton;
+    private JLabel label_preview;
+    private JLabel label_interval;
+    private JSpinner spinner_hours;
+    private JSpinner spinner_minutes;
+    private JSpinner spinner_seconds;
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        String[] strings=args[0].split(":");
-        File path=new File(args[1]);
-        int[] numbers=new int[3];
-        for(int i=0;i<3;i++){
-            numbers[i]=Integer.parseInt(strings[i]);
-        }
+    private boolean run;
+    public MainProgram() {
+        browseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser chooser=new JFileChooser();
+                chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                chooser.setAcceptAllFileFilterUsed(false);
+                chooser.setFileFilter(new FileFilter() {
+                    @Override
+                    public boolean accept(File f) {
+                        String name=f.getName();
+                        if(name.contains("."))
+                            return name.substring(name.lastIndexOf(".")+1).equals("txt");
 
-        Date goalTime=getGoalTime(numbers[0],numbers[1],numbers[2]);
+                        return false;
+                    }
 
-        Calendar calendar=Calendar.getInstance();
-        SimpleDateFormat format= new SimpleDateFormat(args[2]);
+                    @Override
+                    public String getDescription() {
+                        return "*.txt (Text file)";
+                    }
+                });
+                int result=chooser.showDialog(null,null);
+                if(result==JFileChooser.APPROVE_OPTION){
+                    textField_file.setText(chooser.getSelectedFile().toString());
+                    System.out.println(chooser.getSelectedFile());
+                }
 
-        while(goalTime.after(new Date())){
-            calendar.setTime(goalTime);
-            Calendar calendar2 = Calendar.getInstance();
-            calendar.add(Calendar.HOUR_OF_DAY,-calendar2.get(Calendar.HOUR_OF_DAY));
-            calendar.add(Calendar.MINUTE,-calendar2.get(Calendar.MINUTE));
-            calendar.add(Calendar.SECOND,-calendar2.get(Calendar.SECOND));
-            String output=format.format(calendar.getTime());
-            System.out.println(output);
-            try(BufferedWriter writer=new BufferedWriter(new OutputStreamWriter(new FileOutputStream(path,false), StandardCharsets.UTF_8))){
-                writer.write(output);
             }
+        });
 
-            Thread.sleep(1000);
-        }
+        slider_update_interval.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                label_interval.setText(slider_update_interval.getValue()+" ms");
+            }
+        });
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                start();
+            }
+        });
+        stopButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                stop();
+            }
+        });
     }
-    public static Date getGoalTime(int hours,int minutes,int seconds){
+
+    private void start(){
+        final Date goalTime=getGoalTime();
+        final File file=new File(textField_file.getText());
+        final String format_pattern=textField_format.getText();
+        final int update_interval= slider_update_interval.getValue();
+
+        final SimpleDateFormat format= new SimpleDateFormat(format_pattern);
+        Thread thread=generateThread(goalTime,format,file,update_interval);
+        run=true;
+        thread.start();
+        startButton.setEnabled(false);
+        stopButton.setEnabled(true);
+    }
+    private void stop(){
+        run=false;
+        startButton.setEnabled(true);
+        stopButton.setEnabled(false);
+    }
+
+    private Thread generateThread(Date goalTime, SimpleDateFormat format, File file, int update_interval){
+        return new Thread(() -> {
+            while(run&&goalTime.after(new Date())){
+                Date date=getDifference(goalTime);
+                String output=format.format(date);
+                update(output,file);
+                try {
+                    Thread.sleep(update_interval);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+            String output=format.format(getZero());
+            update(output,file);
+
+            stop();
+        });
+    }
+    private void update(String output,File file){
+        label_preview.setText(output);
+        try(BufferedWriter writer=new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file,false), StandardCharsets.UTF_8))){
+            writer.write(output);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+    }
+
+    private Date getDifference(Date goal){
+        Calendar calendar=Calendar.getInstance();
+        calendar.setTime(goal);
+        Calendar current_calendar = Calendar.getInstance();
+        calendar.add(Calendar.HOUR_OF_DAY,-current_calendar.get(Calendar.HOUR_OF_DAY));
+        calendar.add(Calendar.MINUTE,-current_calendar.get(Calendar.MINUTE));
+        calendar.add(Calendar.SECOND,-current_calendar.get(Calendar.SECOND));
+        return calendar.getTime();
+    }
+    private Date getZero(){
+        Calendar calendar=Calendar.getInstance();
+        calendar.set(0, Calendar.JANUARY,0,0,0,0);
+        return calendar.getTime();
+    }
+
+    public static void main(String[] args) {
+        FlatDarkLaf.setup();
+        JFrame frame = new JFrame("OBS Text Utils");
+        frame.setContentPane(new MainProgram().root);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private Date getGoalTime(){
         Calendar calendar =Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
-        calendar.add(Calendar.HOUR_OF_DAY,hours);
-        calendar.add(Calendar.MINUTE,minutes);
-        calendar.add(Calendar.SECOND,seconds);
+        calendar.add(Calendar.HOUR_OF_DAY,(int)spinner_hours.getValue());
+        calendar.add(Calendar.MINUTE,(int)spinner_minutes.getValue());
+        calendar.add(Calendar.SECOND,(int)spinner_seconds.getValue());
         return calendar.getTime();
     }
 }
