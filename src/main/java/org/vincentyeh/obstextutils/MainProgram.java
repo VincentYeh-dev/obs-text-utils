0a59f7ab23b5bcf5f47eb5ef2e859dff8cb6cd0f
VincentYeh-dev <vincent3314534>
2022-09-02 22:25:19 +0800

    Fix maven gui problem.

--- src/main/java/org/vincentyeh/obstextutils/MainProgram.java
+++ src/main/java/org/vincentyeh/obstextutils/MainProgram.java
@@ -1,11 +1,17 @@
 package org.vincentyeh.obstextutils;
 
 import com.formdev.flatlaf.FlatDarkLaf;
+import com.intellij.uiDesigner.core.GridConstraints;
+import com.intellij.uiDesigner.core.GridLayoutManager;
+import com.intellij.uiDesigner.core.Spacer;
 
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.filechooser.FileFilter;
+import javax.swing.plaf.FontUIResource;
+import javax.swing.text.StyleContext;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
@@ -13,6 +19,7 @@ import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class MainProgram {
     private JPanel root;
@@ -29,19 +36,20 @@ public class MainProgram {
     private JSpinner spinner_seconds;
 
     private boolean run;
+
     public MainProgram() {
         browseButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JFileChooser chooser=new JFileChooser();
+                JFileChooser chooser = new JFileChooser();
                 chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
                 chooser.setAcceptAllFileFilterUsed(false);
                 chooser.setFileFilter(new FileFilter() {
                     @Override
                     public boolean accept(File f) {
-                        String name=f.getName();
-                        if(name.contains("."))
-                            return name.substring(name.lastIndexOf(".")+1).equals("txt");
+                        String name = f.getName();
+                        if (name.contains("."))
+                            return name.substring(name.lastIndexOf(".") + 1).equals("txt");
 
                         return false;
                     }
@@ -51,8 +59,8 @@ public class MainProgram {
                         return "*.txt (Text file)";
                     }
                 });
-                int result=chooser.showDialog(null,null);
-                if(result==JFileChooser.APPROVE_OPTION){
+                int result = chooser.showSaveDialog(null);
+                if (result == JFileChooser.APPROVE_OPTION) {
                     textField_file.setText(chooser.getSelectedFile().toString());
                     System.out.println(chooser.getSelectedFile());
                 }
@@ -63,7 +71,7 @@ public class MainProgram {
         slider_update_interval.addChangeListener(new ChangeListener() {
             @Override
             public void stateChanged(ChangeEvent e) {
-                label_interval.setText(slider_update_interval.getValue()+" ms");
+                label_interval.setText(slider_update_interval.getValue() + " ms");
             }
         });
         startButton.addActionListener(new ActionListener() {
@@ -80,46 +88,48 @@ public class MainProgram {
         });
     }
 
-    private void start(){
-        final Date goalTime=getGoalTime();
-        final File file=new File(textField_file.getText());
-        final String format_pattern=textField_format.getText();
-        final int update_interval= slider_update_interval.getValue();
+    private void start() {
+        final Date goalTime = getGoalTime();
+        final File file = new File(textField_file.getText());
+        final String format_pattern = textField_format.getText();
+        final int update_interval = slider_update_interval.getValue();
 
-        final SimpleDateFormat format= new SimpleDateFormat(format_pattern);
-        Thread thread=generateThread(goalTime,format,file,update_interval);
-        run=true;
+        final SimpleDateFormat format = new SimpleDateFormat(format_pattern);
+        Thread thread = generateThread(goalTime, format, file, update_interval);
+        run = true;
         thread.start();
         startButton.setEnabled(false);
         stopButton.setEnabled(true);
     }
-    private void stop(){
-        run=false;
+
+    private void stop() {
+        run = false;
         startButton.setEnabled(true);
         stopButton.setEnabled(false);
     }
 
-    private Thread generateThread(Date goalTime, SimpleDateFormat format, File file, int update_interval){
+    private Thread generateThread(Date goalTime, SimpleDateFormat format, File file, int update_interval) {
         return new Thread(() -> {
-            while(run&&goalTime.after(new Date())){
-                Date date=getDifference(goalTime);
-                String output=format.format(date);
-                update(output,file);
+            while (run && goalTime.after(new Date())) {
+                Date date = getDifference(goalTime);
+                String output = format.format(date);
+                update(output, file);
                 try {
                     Thread.sleep(update_interval);
                 } catch (InterruptedException ex) {
                     ex.printStackTrace();
                 }
             }
-            String output=format.format(getZero());
-            update(output,file);
+            String output = format.format(getZero());
+            update(output, file);
 
             stop();
         });
     }
-    private void update(String output,File file){
+
+    private void update(String output, File file) {
         label_preview.setText(output);
-        try(BufferedWriter writer=new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file,false), StandardCharsets.UTF_8))){
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file, false), StandardCharsets.UTF_8))) {
             writer.write(output);
         } catch (IOException ioException) {
             ioException.printStackTrace();
@@ -127,18 +137,19 @@ public class MainProgram {
 
     }
 
-    private Date getDifference(Date goal){
-        Calendar calendar=Calendar.getInstance();
+    private Date getDifference(Date goal) {
+        Calendar calendar = Calendar.getInstance();
         calendar.setTime(goal);
         Calendar current_calendar = Calendar.getInstance();
-        calendar.add(Calendar.HOUR_OF_DAY,-current_calendar.get(Calendar.HOUR_OF_DAY));
-        calendar.add(Calendar.MINUTE,-current_calendar.get(Calendar.MINUTE));
-        calendar.add(Calendar.SECOND,-current_calendar.get(Calendar.SECOND));
+        calendar.add(Calendar.HOUR_OF_DAY, -current_calendar.get(Calendar.HOUR_OF_DAY));
+        calendar.add(Calendar.MINUTE, -current_calendar.get(Calendar.MINUTE));
+        calendar.add(Calendar.SECOND, -current_calendar.get(Calendar.SECOND));
         return calendar.getTime();
     }
-    private Date getZero(){
-        Calendar calendar=Calendar.getInstance();
-        calendar.set(0, Calendar.JANUARY,0,0,0,0);
+
+    private Date getZero() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(0, Calendar.JANUARY, 0, 0, 0, 0);
         return calendar.getTime();
     }
 
@@ -151,12 +162,129 @@ public class MainProgram {
         frame.setVisible(true);
     }
 
-    private Date getGoalTime(){
-        Calendar calendar =Calendar.getInstance();
+    private Date getGoalTime() {
+        Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
-        calendar.add(Calendar.HOUR_OF_DAY,(int)spinner_hours.getValue());
-        calendar.add(Calendar.MINUTE,(int)spinner_minutes.getValue());
-        calendar.add(Calendar.SECOND,(int)spinner_seconds.getValue());
+        calendar.add(Calendar.HOUR_OF_DAY, (int) spinner_hours.getValue());
+        calendar.add(Calendar.MINUTE, (int) spinner_minutes.getValue());
+        calendar.add(Calendar.SECOND, (int) spinner_seconds.getValue());
         return calendar.getTime();
+    }
+
+    {
+// GUI initializer generated by IntelliJ IDEA GUI Designer
+// >>> IMPORTANT!! <<<
+// DO NOT EDIT OR ADD ANY CODE HERE!
+        $$$setupUI$$$();
+    }
+
+    /**
+     * Method generated by IntelliJ IDEA GUI Designer
+     * >>> IMPORTANT!! <<<
+     * DO NOT edit this method OR call it in your code!
+     *
+     * @noinspection ALL
+     */
+    private void $$$setupUI$$$() {
+        root = new JPanel();
+        root.setLayout(new GridLayoutManager(3, 2, new Insets(10, 10, 10, 10), -1, -1));
+        final JPanel panel1 = new JPanel();
+        panel1.setLayout(new GridLayoutManager(4, 3, new Insets(0, 0, 0, 0), -1, -1));
+        root.add(panel1, new GridConstraints(0, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        final JLabel label1 = new JLabel();
+        label1.setText("File");
+        panel1.add(label1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        textField_file = new JTextField();
+        textField_file.setEnabled(false);
+        panel1.add(textField_file, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        final JLabel label2 = new JLabel();
+        label2.setText("Update Interval");
+        panel1.add(label2, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        slider_update_interval = new JSlider();
+        slider_update_interval.setMaximum(5000);
+        slider_update_interval.setMinimum(100);
+        slider_update_interval.setValue(500);
+        panel1.add(slider_update_interval, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        label_interval = new JLabel();
+        label_interval.setText("500 ms");
+        panel1.add(label_interval, new GridConstraints(1, 2, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        browseButton = new JButton();
+        browseButton.setText("browse");
+        panel1.add(browseButton, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JLabel label3 = new JLabel();
+        label3.setText("Format");
+        panel1.add(label3, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        textField_format = new JTextField();
+        textField_format.setText("HH:mm:ss");
+        panel1.add(textField_format, new GridConstraints(2, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        final JLabel label4 = new JLabel();
+        label4.setText("Timer");
+        panel1.add(label4, new GridConstraints(3, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JPanel panel2 = new JPanel();
+        panel2.setLayout(new GridLayoutManager(1, 6, new Insets(0, 0, 0, 0), -1, -1));
+        panel1.add(panel2, new GridConstraints(3, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        final JLabel label5 = new JLabel();
+        label5.setText("Hours");
+        panel2.add(label5, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        spinner_hours = new JSpinner();
+        panel2.add(spinner_hours, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JLabel label6 = new JLabel();
+        label6.setText("Minutes");
+        panel2.add(label6, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        spinner_minutes = new JSpinner();
+        panel2.add(spinner_minutes, new GridConstraints(0, 3, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JLabel label7 = new JLabel();
+        label7.setText("Seconds");
+        panel2.add(label7, new GridConstraints(0, 4, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        spinner_seconds = new JSpinner();
+        panel2.add(spinner_seconds, new GridConstraints(0, 5, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final Spacer spacer1 = new Spacer();
+        root.add(spacer1, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        final JPanel panel3 = new JPanel();
+        panel3.setLayout(new GridLayoutManager(1, 3, new Insets(0, 0, 0, 0), -1, -1));
+        root.add(panel3, new GridConstraints(2, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        startButton = new JButton();
+        startButton.setText("Start");
+        panel3.add(startButton, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final Spacer spacer2 = new Spacer();
+        panel3.add(spacer2, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        stopButton = new JButton();
+        stopButton.setEnabled(false);
+        stopButton.setText("Stop");
+        panel3.add(stopButton, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        label_preview = new JLabel();
+        Font label_previewFont = this.$$$getFont$$$(null, -1, 72, label_preview.getFont());
+        if (label_previewFont != null) label_preview.setFont(label_previewFont);
+        label_preview.setText("--");
+        root.add(label_preview, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+    }
+
+    /**
+     * @noinspection ALL
+     */
+    private Font $$$getFont$$$(String fontName, int style, int size, Font currentFont) {
+        if (currentFont == null) return null;
+        String resultName;
+        if (fontName == null) {
+            resultName = currentFont.getName();
+        } else {
+            Font testFont = new Font(fontName, Font.PLAIN, 10);
+            if (testFont.canDisplay('a') && testFont.canDisplay('1')) {
+                resultName = fontName;
+            } else {
+                resultName = currentFont.getName();
+            }
+        }
+        Font font = new Font(resultName, style >= 0 ? style : currentFont.getStyle(), size >= 0 ? size : currentFont.getSize());
+        boolean isMac = System.getProperty("os.name", "").toLowerCase(Locale.ENGLISH).startsWith("mac");
+        Font fontWithFallback = isMac ? new Font(font.getFamily(), font.getStyle(), font.getSize()) : new StyleContext().getFont(font.getFamily(), font.getStyle(), font.getSize());
+        return fontWithFallback instanceof FontUIResource ? fontWithFallback : new FontUIResource(fontWithFallback);
+    }
+
+    /**
+     * @noinspection ALL
+     */
+    public JComponent $$$getRootComponent$$$() {
+        return root;
     }
 }
